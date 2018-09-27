2c0db3b9bc90e8b3a096d6763497e57a81910c3c
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2018-09-27 11:37:40 +0300

    disable LED layer indicators

--- keyboards/ergodox_ez/keymaps/stulentsev/keymap.c
+++ keyboards/ergodox_ez/keymaps/stulentsev/keymap.c
@@ -180,48 +180,48 @@ uint32_t layer_state_set_user(uint32_t state) {
         #endif
         break;
       case 1:
-        ergodox_right_led_1_on();
+        // ergodox_right_led_1_on();
         #ifdef RGBLIGHT_COLOR_LAYER_1
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
         #endif
         break;;
       case 2:
-        ergodox_right_led_2_on();
+        // ergodox_right_led_2_on();
         #ifdef RGBLIGHT_COLOR_LAYER_2
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
         #endif
         break;
       case 3:
-        ergodox_right_led_3_on();
+        // ergodox_right_led_3_on();
         #ifdef RGBLIGHT_COLOR_LAYER_3
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
         #endif
         break;
       case 4:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
+        // ergodox_right_led_1_on();
+        // ergodox_right_led_2_on();
         #ifdef RGBLIGHT_COLOR_LAYER_4
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
         #endif
         break;
       case 5:
-        ergodox_right_led_1_on();
-        ergodox_right_led_3_on();
+        // ergodox_right_led_1_on();
+        // ergodox_right_led_3_on();
         #ifdef RGBLIGHT_COLOR_LAYER_5
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
         #endif
         break;
       case 6:
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
+        // ergodox_right_led_2_on();
+        // ergodox_right_led_3_on();
         #ifdef RGBLIGHT_COLOR_LAYER_6
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
         #endif
         break;
       case 7:
-        ergodox_right_led_1_on();
-        ergodox_right_led_2_on();
-        ergodox_right_led_3_on();
+        // ergodox_right_led_1_on();
+        // ergodox_right_led_2_on();
+        // ergodox_right_led_3_on();
         #ifdef RGBLIGHT_COLOR_LAYER_7
           rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
         #endif
